d7b1f864f2d7ec724d40cd3fe80ef976f36095a6
BrenyQT <brendan.quinn2003@gmail.com>
2025-06-24 16:16:31 +0100

    initial commit

---
+++ backend/spotlight-entertainment/src/main/java/com/spotlight/entertainment/spotlight_entertainment/SpotlightEntertainmentApplication.java
@@ -0,0 +1,13 @@
+package com.spotlight.entertainment.spotlight_entertainment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotlightEntertainmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotlightEntertainmentApplication.class, args);
+	}
+
+}
---
+++ backend/spotlight-entertainment/src/test/java/com/spotlight/entertainment/spotlight_entertainment/SpotlightEntertainmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotlight.entertainment.spotlight_entertainment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotlightEntertainmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
